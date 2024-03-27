26af1aebf964b930bb0fecf88b28acadff2e6704
mkoper02 <maciej.koperdowski@gmail.com>
2024-03-27 11:06:56 +0100

    Created services classes and added TODO methods for each class

---
+++ Server/ReasnAPI/ReasnAPI/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using ReasnAPI.Models.Database;
+
+namespace ReasnAPI.Services {
+    public class UserService (ReasnContext context) {
+        private readonly ReasnContext _context = context;
+
+        /* TODO: Create following functions for this class
+         * create
+         * update
+         * delete
+         * get by ID
+         * get list by filter
+         * get all
+         */
+    }
+}
