45b76eec2feaab46851ae5e6b7cba8bc4c13715e
mkoper02 <maciej.koperdowski@gmail.com>
2024-03-29 17:59:38 +0100

    Created basic CRUD methods

--- Server/ReasnAPI/ReasnAPI/Services/ParticipantService.cs
+++ Server/ReasnAPI/ReasnAPI/Services/ParticipantService.cs
@@ -1,16 +1,67 @@
 ﻿using ReasnAPI.Models.Database;
+using ReasnAPI.Models.DTOs;
+using System.Linq.Expressions;
 
 namespace ReasnAPI.Services {
     public class ParticipantService (ReasnContext context) {
         private readonly ReasnContext _context = context;
 
-        /* TODO: Create following functions for this class
-         * create
-         * update
-         * delete
-         * get by ID
-         * get list by filter
-         * get all
-         */
+        // TODO: add method - get all participants in given event
+
+        public ParticipantDto CreateParticipant(ParticipantDto participantDto) {
+            var participant = new Participant() {
+                EventId = participantDto.EventId,
+                UserId = participantDto.UserId,
+                StatusId = participantDto.StatusId
+            };
+
+            _context.Participants.Add(participant);
+            _context.SaveChanges();
+
+            return MapToParticipantDto(participant);
+        }
+
+        public ParticipantDto UpdateParticipant(int participantId, ParticipantDto participantDto) {
+            var participant = _context.Participants.FirstOrDefault(r => r.Id == participantId);
+
+            if (participant == null) {
+                return null;
+            }
+
+            participant.StatusId = participantDto.StatusId;
+
+            _context.SaveChanges();
+
+            return MapToParticipantDto(participant);
+        }
+
+        public void DeleteParticipant(int participantId) {
+            var participant = _context.Participants.FirstOrDefault(r => r.Id == participantId);
+
+            if (participant != null) {
+                _context.Participants.Remove(participant);
+                _context.SaveChanges();
+            }
+        }
+
+        public ParticipantDto GetParticipantById(int participantId) {
+            return MapToParticipantDto(_context.Participants.FirstOrDefault(r => r.Id == participantId));
+        }
+
+        public List<ParticipantDto> GetParticipantsByFilter(Expression<Func<Participant, bool>> filter) {
+            return _context.Participants.Where(filter).Select(participant => MapToParticipantDto(participant)).ToList();
+        }
+
+        public List<ParticipantDto> GetParticipants() {
+            return _context.Participants.Select(participant => MapToParticipantDto(participant)).ToList();
+        }
+
+        private static ParticipantDto MapToParticipantDto(Participant participant) {
+            return new ParticipantDto {
+                EventId = participant.EventId,
+                UserId = participant.UserId,
+                StatusId = participant.StatusId
+            };
+        }
     }
 }
--- Server/ReasnAPI/ReasnAPI/Services/UserService.cs
+++ Server/ReasnAPI/ReasnAPI/Services/UserService.cs
@@ -1,16 +1,89 @@
 ﻿using ReasnAPI.Models.Database;
+using ReasnAPI.Models.DTOs;
+using System.Linq.Expressions;
 
 namespace ReasnAPI.Services {
     public class UserService (ReasnContext context) {
         private readonly ReasnContext _context = context;
 
-        /* TODO: Create following functions for this class
-         * create
-         * update
-         * delete
-         * get by ID
-         * get list by filter
-         * get all
+        /* TODO:
+         * - get user's intrests
+         * - update user's intrests
          */
+
+        public UserDto CreateUser(UserDto userDto) {
+            var user = new User() {
+                Name = userDto.Name,
+                Surname = userDto.Surname,
+                Username = userDto.Username,
+                Email = userDto.Email,
+                Phone = userDto.Phone,
+                RoleId = userDto.RoleId,
+                AddressId = userDto.AddressId,
+
+                CreatedAt = DateTime.Now,
+                IsActive = true
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return userDto;
+        }
+
+        public UserDto UpdateUser(int userId, UserDto userDto) { 
+            var user = _context.Users.FirstOrDefault(r => r.Id == userId);
+
+            if (user == null) {
+                return null;
+            }
+
+            user.Name = userDto.Name;
+            user.Surname = userDto.Surname;
+            user.Username = userDto.Username;
+            user.Email = userDto.Email;
+            user.Phone = userDto.Phone;
+            user.RoleId = userDto.RoleId;
+            user.AddressId = userDto.AddressId;
+
+            user.UpdatedAt = DateTime.Now;
+
+            _context.SaveChanges();
+
+            return MapToUserDto(user);
+        }
+
+        public void DeleteUser(int userId) {
+            var user = _context.Users.FirstOrDefault(r => r.Id == userId);
+
+            if (user != null) { 
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+
+        public UserDto GetUserById(int userId) {
+            return MapToUserDto(_context.Users.FirstOrDefault(r => r.Id == userId));
+        }
+
+        public List<UserDto> GetUsersByFilter(Expression<Func<User, bool>> filter) {
+            return _context.Users.Where(filter).Select(user => MapToUserDto(user)).ToList();
+        }
+
+        public List<UserDto> GetAllUsers() {
+            return _context.Users.Select(user => MapToUserDto(user)).ToList();
+        }
+
+        private static UserDto MapToUserDto(User user) {
+            return new UserDto {
+                Username = user.Username,
+                Name = user.Name,
+                Surname = user.Surname,
+                Email = user.Email,
+                Phone = user.Phone,
+                RoleId = user.RoleId,
+                AddressId = user.AddressId
+            };
+        }
     }
 }
