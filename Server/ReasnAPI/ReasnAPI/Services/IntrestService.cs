aa470889501098835b311a8ebea1e246e2a0dc38
Owczarek Kamil <126111610+bilimig@users.noreply.github.com>
2024-03-30 20:27:03 +0100

    created CRUD Image, Interest, Event and Parameter Services
    Also added geting all elements and by filter(not done properly yet)

--- Server/ReasnAPI/ReasnAPI/Services/IntrestService.cs
+++ Server/ReasnAPI/ReasnAPI/Services/IntrestService.cs
@@ -1,4 +1,5 @@
 ﻿using ReasnAPI.Models.Database;
+using ReasnAPI.Models.DTOs;
 
 namespace ReasnAPI.Services {
     public class IntrestService (ReasnContext context) {
@@ -12,5 +13,103 @@ namespace ReasnAPI.Services {
          * get list by filter
          * get all
          */
+
+        public IntrestDto CreateIntrest(IntrestDto intrestDto)
+        {
+            var newIntrest = new Interest
+            {
+                Name = intrestDto.Name
+            };
+
+            _context.Interests.Add(newIntrest);
+            _context.SaveChanges();
+            return intrestDto;
+        }
+
+
+        public IntrestDto UpdateIntrest(int intrestId,IntrestDto intrestDto)
+        {
+            var intrest = _context.Interests.FirstOrDefault(r => r.Id == intrestId);
+            if (intrest == null)
+            {
+                return null;
+            }
+
+            intrest.Name = intrestDto.Name;
+
+            _context.Interests.Update(intrest);
+            _context.SaveChanges();
+            return intrestDto;
+        }
+
+        public bool DeleteIntrest(int id)
+        {
+            var intrest = _context.Interests.Find(id);
+            if (intrest == null)
+            {
+                return false;
+            }
+
+            _context.Interests.Remove(intrest);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public IntrestDto GetIntrestById(int intrestId)
+        {
+            var intrest = _context.Interests.Find(intrestId);
+            if (intrest == null)
+            {
+                return null;
+            }
+
+            var intrestDto = new IntrestDto
+            {
+                Name = intrest.Name
+            };
+
+            return intrestDto;
+        }
+
+        public List<IntrestDto> GetIntrestsByFilter(string filter)
+        {
+            var intrests = _context.Interests.Where(r => r.Name.Contains(filter)).ToList();
+            if (intrests == null)
+            {
+                return null;
+            }
+
+            var intrestDtos = new List<IntrestDto>();
+            foreach (var intrest in intrests)
+            {
+                intrestDtos.Add(new IntrestDto
+                {
+                    Name = intrest.Name
+                });
+            }
+
+            return intrestDtos;
+        }
+
+        public List<IntrestDto> GetAllIntrests()
+        {
+            var intrests = _context.Interests.ToList();
+            var intrestDtos = new List<IntrestDto>();
+            foreach (var intrest in intrests)
+            {
+                intrestDtos.Add(new IntrestDto
+                {
+                    Name = intrest.Name
+                });
+            }
+
+            return intrestDtos;
+        }
+
+
     }
+
+
+
+
 }
