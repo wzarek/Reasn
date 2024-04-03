0ff6a924aecfcb4ece530e2acae2a158a0694f3d
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2024-04-03 19:06:09 +0200

    Changes after environment reset
    Added Jest
    Added services from RSN-12
    Split .gitignore

---
+++ Client/reasn-client/packages/common/enums/serviceEnums.ts
@@ -0,0 +1,18 @@
+/**
+ * Enum representing HTTP methods.
+ */
+export enum HttpMethod {
+    GET = "GET",
+    POST = "POST",
+    DELETE = "DELETE",
+    PUT = "PUT"
+}
+
+/**
+ * Enum representing available user roles.
+ */
+export enum UserRole {
+    ADMIN = 'admin',
+    USER = 'user',
+    ORGANIZER = 'organizer'
+}
