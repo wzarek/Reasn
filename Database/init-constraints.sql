8a6609dc0d8536aa8d60a64be9847ef4334546d2
rogacky11 <rogacky507@gmail.com>
2024-04-17 23:38:18 +0200

    RSN-23: changed regexes format and added pk for bridge tables

--- Database/init-constraints.sql
+++ Database/init-constraints.sql
@@ -13,46 +13,53 @@ $$ LANGUAGE plpgsql STABLE;
 ALTER TABLE common.image ADD CONSTRAINT chk_object_fk
 CHECK (common.check_fk_exists(object_id, object_type_id));
 
-ALTER TABLE common.address ADD CONSTRAINT chk_address_street CHECK (street ~ '^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+((-| )?[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+((-| )?[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+((-| )?[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)?)?)? \d+([A-Z])?$');
+ALTER TABLE common.address ADD CONSTRAINT chk_address_street CHECK (street ~ '^[[:alnum:]]+(?:(\s)[[:alpha:]]+)*(\s(?:(\d+|\d+/\d+)))?$');
 
-ALTER TABLE common.address ADD CONSTRAINT chk_address_zip_code CHECK (zip_code ~ '^\d{2}-\d{3}$');
+ALTER TABLE common.address ADD CONSTRAINT chk_address_zip_code CHECK (zip_code ~ '^[[:alnum:]\s-]{3,}$');
 
-ALTER TABLE common.address ADD CONSTRAINT chk_address_city CHECK (city ~ '^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+((-| )?[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+((-| )?[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+((-| )?[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)?)?)?$');
+ALTER TABLE common.address ADD CONSTRAINT chk_address_city CHECK (city ~ '^[[:upper:]][[:lower:]]+(?:(\s|-)[[:upper:]][[:lower:]]+)*$');
 
-ALTER TABLE common.address ADD CONSTRAINT chk_address_state CHECK (state ~ '^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(-[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)?$');
+ALTER TABLE common.address ADD CONSTRAINT chk_address_state CHECK (state ~ '^[[:upper:]][[:lower:]]+(?:(\s|-)[[:alpha:]]+)*$');
 
-ALTER TABLE common.address ADD CONSTRAINT chk_address_country CHECK (country ~ '^[A-Z][a-z]+$');
+ALTER TABLE common.address ADD CONSTRAINT chk_address_country CHECK (country ~ '^[[:upper:]][[:lower:]]+(?:\s[[:upper:]][[:lower:]]+){0,1}$');
 
-ALTER TABLE users.user  ADD CONSTRAINT chk_user_email CHECK (email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$');
+ALTER TABLE users.user  ADD CONSTRAINT chk_user_email CHECK (email ~ '^[[:alnum:]._%+-]+@[[:alnum:].-]+\.[[:alpha:]]{2,6}$');
 
-ALTER TABLE users.user ADD CONSTRAINT chk_user_phone CHECK (phone ~ '^\d{9}$');
+ALTER TABLE users.user ADD CONSTRAINT chk_user_phone CHECK (phone ~ '^\+\d{1,3}\s\d{1,15}$');
 
-ALTER TABLE users.user ADD CONSTRAINT chk_user_name CHECK (name ~ '^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻàáâäãåčćèéêëėįìíîïńòôöõøùúûüųūÿýñçčšžÀÁÂÄÃÅČĖÈÉÊËÌÍÎÏĮÒÔÖÕØÙÚÛÜŲŪŸÝÑßÇŒÆČŠŽ∂ð][a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻàáâäãåčćèéêëėįìíîïńòôöõøùúûüųūÿýñçčšžÀÁÂÄÃÅČĖÈÉÊËÌÍÎÏĮÒÔÖÕØÙÚÛÜŲŪŸÝÑßÇŒÆČŠŽ∂ð ''\\-]+$');
+ALTER TABLE users.user ADD CONSTRAINT chk_user_name CHECK (name ~ '^[[:upper:]][[:lower:]]+$');
 
-ALTER TABLE users.user ADD CONSTRAINT chk_user_surname CHECK (surname ~ '^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻàáâäãåčćèéêëėįìíîïńòôöõøùúûüųūÿýñçčšžÀÁÂÄÃÅČĖÈÉÊËÌÍÎÏĮÒÔÖÕØÙÚÛÜŲŪŸÝÑßÇŒÆČŠŽ∂ð][a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻàáâäãåčćèéêëėįìíîïńòôöõøùúûüųūÿýñçčšžÀÁÂÄÃÅČĖÈÉÊËÌÍÎÏĮÒÔÖÕØÙÚÛÜŲŪŸÝÑßÇŒÆČŠŽ∂ð ''\\-]+$');
+ALTER TABLE users.user ADD CONSTRAINT chk_user_surname CHECK (surname ~ '^[[:alpha:]]+((\s)?('')?(-)?)?[[:alpha:]]+$');
 
-ALTER TABLE users.user ADD CONSTRAINT chk_user_username CHECK (username ~ '^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻàáâäãåčćèéêëėįìíîïńòôöõøùúûüųūÿýñçčšžÀÁÂÄÃÅČĖÈÉÊËÌÍÎÏĮÒÔÖÕØÙÚÛÜŲŪŸÝÑßÇŒÆČŠŽ∂ð 0-9 ]+$');
+ALTER TABLE users.user ADD CONSTRAINT chk_user_username CHECK (username ~ '^[[:alnum:]]+$');
 
-ALTER TABLE users.interest ADD CONSTRAINT chk_interest_name CHECK (name ~ '^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż ]+$');
+ALTER TABLE users.interest ADD CONSTRAINT chk_interest_name CHECK (name ~ '^[[:upper:]][[:lower:]]+(?:\s[[:alpha:]]+)*$');
 
-ALTER TABLE events.event ADD CONSTRAINT chk_event_slug CHECK (slug ~ '^[a-z0-9]+(?:-[a-z0-9]+)*$');
+ALTER TABLE events.event ADD CONSTRAINT chk_event_slug CHECK (slug ~ '^[[:alnum:]]+(?:-[[:alnum:]]+)*$');
 
 ALTER TABLE events.event ADD CONSTRAINT check_start_end_time CHECK (start_at < end_at);
 
-ALTER TABLE events.tag ADD CONSTRAINT chk_tag_name CHECK (name ~ '^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż ]+$');
+ALTER TABLE events.tag ADD CONSTRAINT chk_tag_name CHECK (name ~ '^[[:alpha:]]+(?:\s[[:alpha:]]+)*$');
 
-ALTER TABLE events.parameter ADD CONSTRAINT chk_parameter_value CHECK (value ~ '^([A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż0-9]+)(?: [A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż 0-9]+)*$');
+ALTER TABLE events.parameter ADD CONSTRAINT chk_parameter_value CHECK (value ~ '^[[:alnum:]]+(?:\s[[:alnum:]]+)*$');
 
-ALTER TABLE events.parameter ADD CONSTRAINT chk_parameter_key CHECK (key ~ '^([A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż0-9]+)(?: [A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż 0-9]+)*$');
+ALTER TABLE events.parameter ADD CONSTRAINT chk_parameter_key CHECK (key ~ '^[[:alpha:]]+(?:\s[[:alpha:]]+)*$');
 
 ALTER TABLE users.role ADD CONSTRAINT chk_three_roles CHECK (id <= 3);
 
 ALTER TABLE users.role ADD CONSTRAINT chk_role_name CHECK (name ~ '^(Organizer|Admin|User)$');
 
-ALTER TABLE common.status ADD CONSTRAINT chk_four_statuses CHECK (id <= 4);
+ALTER TABLE common.status ADD CONSTRAINT chk_five_statuses CHECK (id <= 5);
 
-ALTER TABLE common.status ADD CONSTRAINT chk_status_name  CHECK (name ~ '^(Interested|Participating|Completed|In progress)$');
+ALTER TABLE common.status ADD CONSTRAINT chk_status_name  CHECK (name ~ '^(Interested|Participating|Completed|In progress|Waiting for approval)$');
 
 ALTER TABLE common.object_type ADD CONSTRAINT chk_two_object_types CHECK (id <= 2);
 
 ALTER TABLE common.object_type ADD CONSTRAINT chk_object_type_name CHECK (name ~ '^(Event|User)$');
+
+ALTER TABLE common.status
+ADD CONSTRAINT chk_object_type_for_status
+CHECK (
+    (name IN ('Interested', 'Participating') AND object_type_id = 2) OR
+    (name IN ('Completed', 'In progress', 'Waiting for approval') AND object_type_id = 1)
+);
