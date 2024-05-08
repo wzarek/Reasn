8aa1315bd19f2ae0b1b8fff84ba282ae8cd050d5
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2024-05-08 20:04:08 +0200

    feat: hero section base component (like really BASE)

---
+++ Client/reasn-client/packages/ui/src/components/main/HeroSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HeroSection = () => {
+  return (
+    <div className='bg-red-500'>HeroSection</div>
+  )
+}
--- Client/reasn-client/packages/ui/src/index.tsx
+++ Client/reasn-client/packages/ui/src/index.tsx
@@ -1,2 +1,3 @@
 export { Button, type ButtonProps } from "./button";
 export { Card, type CardProps, CardVariant } from "./components/Card";
+export { HeroSection } from "./components/main/HeroSection";
