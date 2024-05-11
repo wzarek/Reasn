29e69e003917fbbe75347389600c1182e49938c6
Maciej Koperdowski <85642918+mkoper02@users.noreply.github.com>
2024-05-11 21:48:49 +0200

    [RSN-45] - Change parameters architecture, replaced role, status, object type with enum (#31)
    * Changed role, status and object_type tables into enum
    
    * Update init-dev-data.sql
    
    * Added status constraints for event and participant tables.

--- Database/init-db.sql
+++ Database/init-db.sql
@@ -2,6 +2,10 @@ CREATE SCHEMA events;
 CREATE SCHEMA users;
 CREATE SCHEMA common;
 
+CREATE TYPE common.status AS ENUM ('Interested', 'Participating', 'Completed', 'In progress', 'Waiting for approval');
+CREATE TYPE users.role AS ENUM ('User', 'Organizer', 'Admin');
+CREATE TYPE common.object_type AS ENUM ('Event', 'User');
+
 CREATE TABLE IF NOT EXISTS events.event (
   "id" SERIAL PRIMARY KEY,
   "name" text NOT NULL CONSTRAINT events_event_name_maxlength CHECK (LENGTH("name") <= 64),
@@ -13,21 +17,15 @@ CREATE TABLE IF NOT EXISTS events.event (
   "created_at" timestamptz NOT NULL,
   "updated_at" timestamptz NOT NULL,
   "slug" text NOT NULL CONSTRAINT events_event_slug_maxlength CHECK (LENGTH("slug") <= 128),
-  "status_id" integer NOT NULL
+  "status" common.status NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS events.participant (
   "id" SERIAL PRIMARY KEY,
   "event_id" integer NOT NULL,
   "user_id" integer NOT NULL,
-  "status_id" integer NOT NULL,
+  "status" common.status NOT NULL,
   UNIQUE ("event_id", "user_id")
-);
-
-CREATE TABLE IF NOT EXISTS common.status (
-  "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULL CONSTRAINT common_status_name_maxlength CHECK (LENGTH("name") <= 32) UNIQUE,
-  "object_type_id" integer NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS events.tag (
@@ -49,7 +47,7 @@ CREATE TABLE IF NOT EXISTS users.user (
   "password" text NOT NULL,
   "created_at" timestamptz NOT NULL,
   "updated_at" timestamptz NOT NULL,
-  "role_id" integer NOT NULL,
+  "role" users.role NOT NULL,
   "email" text NOT NULL  CONSTRAINT users_user_email_maxlength CHECK (LENGTH("email") <= 255) UNIQUE,
   "is_active" boolean NOT NULL,
   "address_id" integer NOT NULL,
@@ -67,11 +65,6 @@ CREATE TABLE IF NOT EXISTS events.event_parameter (
   "parameter_id" integer NOT NULL,
   "event_id" integer NOT NULL,
   PRIMARY KEY (parameter_id, event_id)
-);
-
-CREATE TABLE IF NOT EXISTS users.role (
-  "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULL CHECK (LENGTH("name") <= 32) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS events.comment (
@@ -94,13 +87,8 @@ CREATE TABLE IF NOT EXISTS common.address (
 CREATE TABLE IF NOT EXISTS common.image (
   "id" SERIAL PRIMARY KEY,
   "image_data" bytea NOT NULL,
-  "object_type_id" integer NOT NULL,
+  "object_type" common.object_type NOT NULL,
   "object_id" integer  NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS common.object_type (
-  "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULL CONSTRAINT common_object_type_name_maxlength CHECK (LENGTH("name") <= 32) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS users.user_interest (
@@ -115,8 +103,6 @@ CREATE TABLE IF NOT EXISTS users.interest (
   "name" text NOT NULL CONSTRAINT users_interest_name_maxlength CHECK (LENGTH("name") <= 32) UNIQUE
 );
 
-ALTER TABLE users.user ADD FOREIGN KEY ("role_id") REFERENCES users.role ("id");
-
 ALTER TABLE events.event ADD FOREIGN KEY ("address_id") REFERENCES common.address ("id");
 
 ALTER TABLE events.comment ADD FOREIGN KEY ("event_id") REFERENCES events.event ("id");
@@ -129,13 +115,9 @@ ALTER TABLE events.participant ADD FOREIGN KEY ("user_id") REFERENCES users.user
 
 ALTER TABLE events.comment ADD FOREIGN KEY ("user_id") REFERENCES users.user ("id");
 
-ALTER TABLE common.image ADD FOREIGN KEY ("object_type_id") REFERENCES common.object_type ("id");
-
 ALTER TABLE users.user_interest ADD FOREIGN KEY ("user_id") REFERENCES users.user ("id");
 
 ALTER TABLE users.user ADD FOREIGN KEY ("address_id") REFERENCES common.address ("id");
-
-ALTER TABLE events.participant ADD FOREIGN KEY ("status_id") REFERENCES common.status ("id");
 
 ALTER TABLE events.event_parameter ADD FOREIGN KEY ("parameter_id") REFERENCES events.parameter ("id");
 
@@ -145,31 +127,8 @@ ALTER TABLE events.event_tag ADD FOREIGN KEY ("tag_id") REFERENCES events.tag ("
 
 ALTER TABLE events.event_tag ADD FOREIGN KEY ("event_id") REFERENCES events.event ("id");
 
-ALTER TABLE common.status ADD FOREIGN KEY ("object_type_id") REFERENCES common.object_type ("id");
-
-ALTER TABLE events.event ADD FOREIGN KEY ("status_id") REFERENCES common.status ("id");
-
 ALTER TABLE users.user_interest ADD FOREIGN KEY ("interest_id") REFERENCES users.interest ("id");
-
-INSERT INTO users.role ("id", "name") VALUES
-(1, 'User'),
-(2, 'Organizer'),
-(3, 'Admin');
-SELECT setval('common.address_id_seq', (SELECT MAX(id) FROM users.role));
-
-INSERT INTO common.object_type ("id", "name") VALUES 
-(1, 'Event'),
-(2, 'User');
-SELECT setval('common.address_id_seq', (SELECT MAX(id) FROM common.object_type));
-
-INSERT INTO common.status ("id", "name", "object_type_id") VALUES
-(1, 'Interested', 2),
-(2, 'Participating', 2),
-(3, 'Completed', 1),
-(4, 'In progress', 1),
-(5, 'Waiting for approval', 1);
-SELECT setval('common.address_id_seq', (SELECT MAX(id) FROM common.status));
-
+  
 INSERT INTO events.tag ("id", "name") VALUES
 (1, 'Technologia'),
 (2, 'Sport'),
