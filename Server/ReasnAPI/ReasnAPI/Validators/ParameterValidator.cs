be3fc4757d0c13d1428be31036751f10d2aa6484
Maciej Koperdowski <maciej.koperdowski@gmail.com>
2024-05-15 18:20:23 +0200

    Created const variables for regex strings and hardcoded numbers

--- Server/ReasnAPI/ReasnAPI/Validators/ParameterValidator.cs
+++ Server/ReasnAPI/ReasnAPI/Validators/ParameterValidator.cs
@@ -6,6 +6,11 @@ namespace ReasnAPI.Validators
 {
     public class ParameterValidator : IValidator<ParameterDto>
     {
+        private const int KeyMaxLength = 32;
+        private const string KeyRegexPattern = "^\\p{L}+(?:\\s\\p{L}+)*$";
+        private const int ValueMaxLength = 64;
+        private const string ValueRegexPattern = "^[\\p{L}\\d]+(?:\\s[\\p{L}\\d]+)*$";
+
         public static IEnumerable<ValidationResult> Validate(ParameterDto parameter)
         {
             if (string.IsNullOrWhiteSpace(parameter.Key))
@@ -13,12 +18,12 @@ namespace ReasnAPI.Validators
                 yield return new ValidationResult("Key is required", [nameof(parameter.Key)]);
             }
 
-            if (parameter.Key.Length > 32)
+            if (parameter.Key.Length > KeyMaxLength)
             {
                 yield return new ValidationResult("Key is too long", [nameof(parameter.Key)]);
             }
 
-            if (!new Regex("^\\p{L}+(?:\\s\\p{L}+)*$").IsMatch(parameter.Key))
+            if (!new Regex(KeyRegexPattern).IsMatch(parameter.Key))
             {
                 yield return new ValidationResult("Key is invalid", [nameof(parameter.Key)]);
             }
@@ -28,12 +33,12 @@ namespace ReasnAPI.Validators
                 yield return new ValidationResult("Value is required", [nameof(parameter.Value)]);
             }
 
-            if (parameter.Value.Length > 64)
+            if (parameter.Value.Length > ValueMaxLength)
             {
                 yield return new ValidationResult("Value is too long", [nameof(parameter.Value)]);
             }
 
-            if (!new Regex("^[\\p{L}\\d]+(?:\\s[\\p{L}\\d]+)*$").IsMatch(parameter.Value))
+            if (!new Regex(ValueRegexPattern).IsMatch(parameter.Value))
             {
                 yield return new ValidationResult("Value is invalid", [nameof(parameter.Value)]);
             }
