729fb058c3944112cc713d3f29860ea5b0841e73
raczu <Raczuekk@gmail.com>
2024-06-01 20:56:45 +0200

    test: add unit tests for jwt, handlers and validators

--- Server/ReasnAPI/ReasnAPI.Tests/UnitTests/Services/Authentication/AuthServiceTests.cs
+++ Server/ReasnAPI/ReasnAPI.Tests/UnitTests/Services/Authentication/AuthServiceTests.cs
@@ -23,6 +23,7 @@ public class AuthServiceTests
         _mockContext = new Mock<ReasnContext>();
         _hasher = new PasswordHasher<User>();
         _service = new AuthService(_mockContext.Object);
+
         var user = new User
         {
             Email = "jon.snow@castleblack.com",
@@ -104,7 +105,7 @@ public class AuthServiceTests
             Username = "jstark",
             Phone = "+123 456789"
         };
-
+        
         Assert.ThrowsException<BadRequestException>(() => _service.Register(request));
     }
 
