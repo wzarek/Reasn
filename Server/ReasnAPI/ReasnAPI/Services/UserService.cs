fd8fa07b25642c9a09830cbf91439b28cc5b80d7
Maciej Koperdowski <maciej.koperdowski@gmail.com>
2024-06-06 20:20:53 +0200

    Transaction for user update service

--- Server/ReasnAPI/ReasnAPI/Services/UserService.cs
+++ Server/ReasnAPI/ReasnAPI/Services/UserService.cs
@@ -1,163 +1,107 @@
 using ReasnAPI.Models.Database;
 using ReasnAPI.Models.DTOs;
 using System.Linq.Expressions;
+using System.Transactions;
 
 namespace ReasnAPI.Services
 {
     public class UserService(ReasnContext context)
     {
-        public UserDto? CreateUser(UserDto? userDto)
-        {
-            if (userDto is null)
-            {
-                return null;
-            }
-
-            var userDb = context.Users.FirstOrDefault(r => r.Username == userDto.Username);
-
-            if (userDb is not null)
-            {
-                return null;
-            }
-
-            Console.WriteLine(userDto.Role);
-
-            var user = new User
-            {
-                Name = userDto.Name,
-                Surname = userDto.Surname,
-                Username = userDto.Username,
-                Email = userDto.Email,
-                Phone = userDto.Phone,
-                Role = userDto.Role,
-                AddressId = userDto.AddressId,
-                CreatedAt = DateTime.UtcNow,
-                IsActive = true
-            };
-
-            context.Users.Add(user);
-
-            if (userDto.Interests is null)
-            {
-                context.SaveChanges();
-                return userDto;
-            }
-
-            foreach (var interest in userDto.Interests)
-            {
-                var interestDb = context.Interests.FirstOrDefault(i => i.Name == interest.Interest.Name);
-
-                if (interestDb is null)
-                {
-                    continue;
-                }
-
-                var newInterest = new UserInterest
-                {
-                    UserId = user.Id,
-                    InterestId = interestDb.Id,
-                    Level = interest.Level
-                };
-
-                context.UserInterests.Add(newInterest);
-            }
-
-            context.SaveChanges();
-
-            return userDto;
-        }
-
         public UserDto? UpdateUser(int userId, UserDto? userDto)
         {
-            if (userDto is null)
+            using (var scope = new TransactionScope())
             {
-                return null;
-            }
-
-            var user = context.Users.FirstOrDefault(r => r.Id == userId);
-
-            if (user is null)
-            {
-                return null;
-            }
-
-            var usernameExists = context.Users.Any(r => r.Username == userDto.Username && r.Id != userId);
-
-            if (usernameExists)
-            {
-                return null;
-            }
-
-            user.Username = userDto.Username;
-            user.Name = userDto.Name;
-            user.Surname = userDto.Surname;
-            user.Username = userDto.Username;
-            user.Email = userDto.Email;
-            user.Phone = userDto.Phone;
-            user.Role = userDto.Role;
-            user.AddressId = userDto.AddressId;
-            user.UpdatedAt = DateTime.UtcNow;
-
-            context.Users.Update(user);
-
-            if (userDto.Interests is null || userDto.Interests.Count == 0)
-            {
-                context.SaveChanges();
-                return MapToUserDto(user);
-            }
-
-            var userInterestsDb = context.UserInterests
-                                       .Where(ui => ui.UserId == userId)
-                                       .ToList();
-
-            foreach (var userInterest in userInterestsDb)
-            {
-                var interestDb = context.Interests.Find(userInterest.InterestId);
-
-                if (interestDb is null)
+                if (userDto is null)
                 {
-                    continue;
+                    return null;
                 }
 
-                if (userDto.Interests.Exists(i => i.Interest.Name == interestDb.Name))
-                {
-                    var userInterestDto = userDto.Interests.Find(i => i.Interest.Name == interestDb.Name);   
+                var user = context.Users.FirstOrDefault(r => r.Id == userId);
 
-                    if (userInterestDto is null)
+                if (user is null)
+                {
+                    return null;
+                }
+
+                var usernameExists = context.Users.Any(r => r.Username == userDto.Username && r.Id != userId);
+
+                if (usernameExists)
+                {
+                    return null;
+                }
+
+                user.Username = userDto.Username;
+                user.Name = userDto.Name;
+                user.Surname = userDto.Surname;
+                user.Username = userDto.Username;
+                user.Email = userDto.Email;
+                user.Phone = userDto.Phone;
+                user.Role = userDto.Role;
+                user.AddressId = userDto.AddressId;
+                user.UpdatedAt = DateTime.UtcNow;
+
+                context.Users.Update(user);
+
+                if (userDto.Interests is null || userDto.Interests.Count == 0)
+                {
+                    context.SaveChanges();
+                    return MapToUserDto(user);
+                }
+
+                var userInterestsDb = context.UserInterests
+                                           .Where(ui => ui.UserId == userId)
+                                           .ToList();
+
+                foreach (var userInterest in userInterestsDb)
+                {
+                    var interestDb = context.Interests.Find(userInterest.InterestId);
+
+                    if (interestDb is null)
                     {
                         continue;
                     }
 
-                    userInterest.Level = userInterestDto.Level;
-                    context.UserInterests.Update(userInterest);
-                    userDto.Interests.RemoveAll(i => i.Interest.Name == interestDb.Name);
+                    if (userDto.Interests.Exists(i => i.Interest.Name == interestDb.Name))
+                    {
+                        var userInterestDto = userDto.Interests.Find(i => i.Interest.Name == interestDb.Name);   
+
+                        if (userInterestDto is null)
+                        {
+                            continue;
+                        }
+
+                        userInterest.Level = userInterestDto.Level;
+                        context.UserInterests.Update(userInterest);
+                        userDto.Interests.RemoveAll(i => i.Interest.Name == interestDb.Name);
+                    }
+                    else
+                    {
+                        context.UserInterests.Remove(userInterest);
+                    }
                 }
-                else
+
+                foreach (var interest in userDto.Interests)
                 {
-                    context.UserInterests.Remove(userInterest);
+                    var interestDb = context.Interests.FirstOrDefault(i => i.Name == interest.Interest.Name);
+
+                    if (interestDb is null)
+                    {
+                        continue;
+                    }
+
+                    var newInterest = new UserInterest
+                    {
+                        UserId = user.Id,
+                        InterestId = interestDb.Id,
+                        Level = interest.Level
+                    };
+
+                    context.UserInterests.Add(newInterest);
                 }
+
+                context.SaveChanges();
+                scope.Complete();
             }
-
-            foreach (var interest in userDto.Interests)
-            {
-                var interestDb = context.Interests.FirstOrDefault(i => i.Name == interest.Interest.Name);
-
-                if (interestDb is null)
-                {
-                    continue;
-                }
-
-                var newInterest = new UserInterest
-                {
-                    UserId = user.Id,
-                    InterestId = interestDb.Id,
-                    Level = interest.Level
-                };
-
-                context.UserInterests.Add(newInterest);
-            }
-
-            context.SaveChanges();
 
             return GetUserById(userId);
         }
