1f272df29fef4e7ae0786f1a27c0c0f730cac970
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2024-06-11 22:05:19 +0200

    feat: implemented base user info page (for me and for user/username)

---
+++ Client/reasn-client/apps/web/app/me/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { SharedUserPage } from "@reasn/ui/src/components/web";
+
+const MePage = () => {
+  return <SharedUserPage username="memememe" />;
+};
+
+export default MePage;
