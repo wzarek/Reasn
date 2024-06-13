9c616dac6828fb7fce751c75cde8a7b93ad1f727
Owczarek Kamil <bilimig@wp.pl>
2024-06-13 12:40:25 +0200

    Created EventsController

--- Server/ReasnAPI/ReasnAPI/Controllers/EventsController.cs
+++ Server/ReasnAPI/ReasnAPI/Controllers/EventsController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ReasnAPI.Models.Database;
+using ReasnAPI.Models.DTOs;
+using ReasnAPI.Models.Enums;
+using ReasnAPI.Services;
+using ReasnAPI.Services.Exceptions;
 
 namespace ReasnAPI.Controllers;
 
@@ -9,6 +13,10 @@ namespace ReasnAPI.Controllers;
 public class EventsController : ControllerBase
 {
     private readonly ReasnContext _context;
+    private readonly EventService _eventService;
+    private readonly UserService _userService;
+    private readonly ImageService _imageService;
+    private readonly TagService _tagService;
 
     public EventsController(ReasnContext context)
     {
@@ -16,63 +24,145 @@ public class EventsController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType<IEnumerable<EventDto>>(StatusCodes.Status200OK)]
     public IActionResult GetEvents()
     {
-        throw new NotImplementedException();
+        var events = _eventService.GetEvents();
+        return Ok(events);
     }
 
     [HttpPost]
     [Authorize(Roles = "Admin, Organizer")]
-    public IActionResult CreateEvent()
+    [ProducesResponseType<EventDto>(StatusCodes.Status201Created)]
+    public IActionResult CreateEvent([FromBody] EventDto eventDto)
     {
-        throw new NotImplementedException();
+        if (eventDto.OrganizerId != _userService.GetCurrentUser().Id)
+        {
+            return Unauthorized();
+        }
+        
+        var createdEvent = _eventService.CreateEvent(eventDto);
+        return Created();
+    
     }
 
     [HttpGet]
     [Route("{slug}")]
+    [ProducesResponseType<EventDto>(StatusCodes.Status200OK)]
     public IActionResult GetEventBySlug(string slug)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var eventDto = _eventService.GetEventBySlug(slug);
+            return Ok(eventDto);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
     }
 
     [HttpPut]
     [Authorize(Roles = "Admin, Organizer")]
     [Route("{slug}")]
-    public IActionResult UpdateEvent(string slug)
+    [ProducesResponseType<EventDto>(StatusCodes.Status200OK)]
+    public IActionResult UpdateEvent(string slug, [FromBody] EventDto eventDto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var existingEvent = _eventService.GetEventBySlug(slug);
+            if (existingEvent == null)
+            {
+                return NotFound();
+            }
+
+            var updatedEvent = _eventService.UpdateEvent(existingEvent.Id, eventDto);
+            return Ok(updatedEvent);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
     }
 
     [HttpGet]
     [Authorize(Roles = "Admin")]
     [Route("requests")]
+    [ProducesResponseType<EventDto>(StatusCodes.Status200OK)]
     public IActionResult GetEventsRequests(string slug)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var events = _eventService.GetEventsByFilter(e => e.Status == "NotApproved");
+            return Ok(events);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 
     [HttpPost]
     [Authorize(Roles = "Admin")]
     [Route("{slug}/approve")]
+    [ProducesResponseType<EventDto>(StatusCodes.Status200OK)]
     public IActionResult ApproveEventRequest(string slug)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var eventToApprove = _eventService.GetEventBySlug(slug);
+
+            eventToApprove.Status = "Approved";
+            _eventService.UpdateEvent(eventToApprove.Id, eventToApprove);
+            return Ok(eventToApprove);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
     }
 
     [HttpPost]
     [Authorize(Roles = "Admin, Organizer")]
     [Route("{slug}/images")]
-    public IActionResult AddEventImage(string slug)
+    public IActionResult AddEventImage(string slug, [FromBody] List<ImageDto> imageDtos)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var @event = _eventService.GetEventBySlug(slug);
+            if (@event.Id != imageDtos[0].ObjectId)
+            {
+                return NotFound(); // jakies exception tutaj
+            }
+            var image = _imageService.CreateImage(imageDtos, ObjectType.Event);
+            return Ok(image);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
+        
     }
 
     [HttpPut]
     [Authorize(Roles = "Admin, Organizer")]
     [Route("{slug}/images/{imageId:int}")]
-    public IActionResult UpdateEventImage(string slug, int imageId)
+    public IActionResult UpdateEventImage(string slug,[FromBody] List<ImageDto> imageDtos)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var @event = _eventService.GetEventBySlug(slug);
+            if (@event.Id != imageDtos[0].ObjectId)
+            {
+                return NotFound(); // jakies exception tutaj
+            }
+            var image = _imageService.UpdateImages(imageDtos, ObjectType.Event);
+            return Ok(image);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
     }
 
     [HttpDelete]
@@ -80,7 +170,7 @@ public class EventsController : ControllerBase
     [Route("{slug}/images/{imageId:int}")]
     public IActionResult DeleteEventImage(string slug, int imageId)
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException(); // pytanie czy to potrzebne zadziala na updatcie XD
     }
 
     [HttpGet]
@@ -142,7 +232,21 @@ public class EventsController : ControllerBase
     [Route("parameters")]
     public IActionResult GetEventsParameters(string slug)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var eventDto = _eventService.GetEventBySlug(slug);
+            if (eventDto == null)
+            {
+                return NotFound();
+            }
+
+            var parameters = eventDto.Parameters;
+            return Ok(parameters);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
     }
 
     [HttpDelete]
@@ -155,10 +259,24 @@ public class EventsController : ControllerBase
 
     [HttpGet]
     [Authorize(Roles = "Admin, Organizer")]
-    [Route("tags")]
+    [Route("{slug}/tags")]
     public IActionResult GetEventsTags(string slug)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var eventDto = _eventService.GetEventBySlug(slug);
+            if (eventDto == null)
+            {
+                return NotFound();
+            }
+
+            var tags = eventDto.Tags;
+            return Ok(tags);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
     }
 
     [HttpDelete]
@@ -166,6 +284,18 @@ public class EventsController : ControllerBase
     [Route("tags/{tagId:int}")]
     public IActionResult DeleteEventsTag(int tagId)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var tag = _tagService.DeleteTag(tagId);
+            return Ok(tag);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
+        catch (BadRequestException)
+        {
+            return BadRequest();
+        }
     }
 }
